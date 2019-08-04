15583aa99dcfd20d9fc219cbe8e972aa8cb95577
Chris <csgephardt@gmail.com>
2019-08-04 06:37:51 -0400

    Updated Files for 0.8.9

--- app/src/main/java/com/MartyrPher/smapiandroidinstaller/BackgroundTask.java
+++ app/src/main/java/com/MartyrPher/smapiandroidinstaller/BackgroundTask.java
@@ -13,7 +13,17 @@ public class BackgroundTask extends AsyncTask<Void, Integer, Boolean> {
     private static final String ASSET_STARDEW_FILES = "Stardew";
     private static final String MOD_FILES_VK = "VirtualKeyboard";
     private static final String MOD_FILES_VK_ASSET = "VirtualKeyboard/assets";
+    private static final String MIPMAP_XXXHDPI_ASSET = "mipmap-xxxhdpi-v4";
+    private static final String MIPMAP_XXHDPI_ASSET = "mipmap-xxhdpi-v4";
+    private static final String MIPMAP_XHDPI_ASSET = "mipmap-xhdpi-v4";
+    private static final String MIPMAP_MDPI_ASSET = "mipmap-mdpi";
+    private static final String MIPMAP_HDPI_ASSET = "mipmap-hdpi";
     private static final String DIR_APK_FILES = "/SMAPI Installer/ApkFiles/";
+    private static final String DIR_APK_FILES_XXXHDPI = "/SMAPI Installer/ApkFiles/mipmap-xxxhdpi-v4/";
+    private static final String DIR_APK_FILES_XXHDPI = "/SMAPI Installer/ApkFiles/mipmap-xxhdpi-v4/";
+    private static final String DIR_APK_FILES_XHDPI = "/SMAPI Installer/ApkFiles/mipmap-xhdpi-v4/";
+    private static final String DIR_APK_FILES_MDPI = "/SMAPI Installer/ApkFiles/mipmap-mdpi/";
+    private static final String DIR_APK_FILES_HDPI = "/SMAPI Installer/ApkFiles/mipmap-hdpi/";
     private static final String DIR_STARDEW_FILES = "/StardewValley/smapi-internal/";
     private static final String DIR_MODS_VK = "/StardewValley/Mods/VirtualKeyboard/";
     private static final String DIR_MODS_VK_ASSET = "/StardewValley/Mods/VirtualKeyboard/assets";
@@ -81,16 +91,107 @@ public class BackgroundTask extends AsyncTask<Void, Integer, Boolean> {
             copy.copyAssets(MOD_FILES_VK_ASSET, DIR_MODS_VK_ASSET);
             publishProgress(45);
 
-            File[] moddingAPI = {new File(Environment.getExternalStorageDirectory() + DIR_APK_FILES + "AndroidManifest.xml"),
-                                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "classes.dex"),
-                                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "StardewModdingAPI.dll"),
-                                new File(Environment.getExternalStorageDirectory() + DIR_APK_FILES + "StardewModdingAPI.Toolkit.CoreInterfaces.dll"),
-                                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "StardewModdingAPI.Toolkit.dll"),
-                                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "Newtonsoft.json.dll"),
-                                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "System.Data.dll"),
-                                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "System.Numerics.dll")};
-            boolean[] compressed = {true, true, false, false, false, false, false, false};
-            String[] paths = {"", "", "assemblies/", "assemblies/", "assemblies/", "assemblies/", "assemblies/", "assemblies/"};
+            copy.copyAssets(MIPMAP_XXXHDPI_ASSET, DIR_APK_FILES_XXXHDPI);
+            copy.copyAssets(MIPMAP_XXHDPI_ASSET, DIR_APK_FILES_XXHDPI);
+            copy.copyAssets(MIPMAP_XHDPI_ASSET, DIR_APK_FILES_XHDPI);
+            copy.copyAssets(MIPMAP_MDPI_ASSET, DIR_APK_FILES_MDPI);
+            copy.copyAssets(MIPMAP_HDPI_ASSET, DIR_APK_FILES_HDPI);
+
+            File[] moddingAPI =
+            {
+                new File(Environment.getExternalStorageDirectory() + DIR_APK_FILES + "AndroidManifest.xml"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "classes.dex"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "StardewModdingAPI.dll"),
+                new File(Environment.getExternalStorageDirectory() + DIR_APK_FILES + "StardewModdingAPI.Toolkit.CoreInterfaces.dll"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "StardewModdingAPI.Toolkit.dll"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "Newtonsoft.json.dll"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "System.Data.dll"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES + "System.Numerics.dll"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXXHDPI + "ic_launcher.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXXHDPI + "ic_launcher_background.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXXHDPI + "ic_launcher_foreground.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXXHDPI + "ic_launcher_round.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXHDPI + "ic_launcher.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXHDPI + "ic_launcher_background.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXHDPI + "ic_launcher_foreground.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XXHDPI + "ic_launcher_round.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XHDPI + "ic_launcher.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XHDPI + "ic_launcher_background.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XHDPI + "ic_launcher_foreground.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_XHDPI + "ic_launcher_round.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_MDPI + "ic_launcher.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_MDPI + "ic_launcher_background.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_MDPI + "ic_launcher_foreground.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_MDPI + "ic_launcher_round.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_HDPI + "ic_launcher.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_HDPI + "ic_launcher_background.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_HDPI + "ic_launcher_foreground.png"),
+                new File( Environment.getExternalStorageDirectory() + DIR_APK_FILES_HDPI + "ic_launcher_round.png")
+            };
+
+            boolean[] compressed =
+            {
+                true,
+                true,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true
+            };
+
+            String[] paths =
+            {
+                "",
+                "",
+                "assemblies/",
+                "assemblies/",
+                "assemblies/",
+                "assemblies/",
+                "assemblies/",
+                "assemblies/",
+                "res/mipmap-xxxhdpi-v4/",
+                "res/mipmap-xxxhdpi-v4/",
+                "res/mipmap-xxxhdpi-v4/",
+                "res/mipmap-xxxhdpi-v4/",
+                "res/mipmap-xxhdpi-v4/",
+                "res/mipmap-xxhdpi-v4/",
+                "res/mipmap-xxhdpi-v4/",
+                "res/mipmap-xxhdpi-v4/",
+                "res/mipmap-xhdpi-v4/",
+                "res/mipmap-xhdpi-v4/",
+                "res/mipmap-xhdpi-v4/",
+                "res/mipmap-xhdpi-v4/",
+                "res/mipmap-mdpi/",
+                "res/mipmap-mdpi/",
+                "res/mipmap-mdpi/",
+                "res/mipmap-mdpi/",
+                "res/mipmap-hdpi/",
+                "res/mipmap-hdpi/",
+                "res/mipmap-hdpi/",
+                "res/mipmap-hdpi/"
+            };
             publishProgress(54);
 
             writeApk.addFilesToApk(new File(Environment.getExternalStorageDirectory() + "/SMAPI Installer/base.apk"), moddingAPI, paths, compressed);
