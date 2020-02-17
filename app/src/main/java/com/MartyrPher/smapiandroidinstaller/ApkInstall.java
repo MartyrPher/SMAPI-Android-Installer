d720b3d28b41ad7b9ede4b10c90e5731ccdcdc96
Chris <csgephardt@gmail.com>
2020-02-17 00:26:03 -0500

    Update Installer to AndroidX

--- app/src/main/java/com/MartyrPher/smapiandroidinstaller/ApkInstall.java
+++ app/src/main/java/com/MartyrPher/smapiandroidinstaller/ApkInstall.java
@@ -6,8 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.support.v4.content.FileProvider;
-
+import androidx.core.content.FileProvider;
 import java.io.File;
 
 /**
--- app/src/main/java/com/MartyrPher/smapiandroidinstaller/DialogFrag.java
+++ app/src/main/java/com/MartyrPher/smapiandroidinstaller/DialogFrag.java
@@ -1,20 +1,13 @@
 package com.MartyrPher.smapiandroidinstaller;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 /**
  * The dialog that is shown in different situations
